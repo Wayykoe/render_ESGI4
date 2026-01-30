5d6af9f56162685c1f9c006d064583ae05438361
Wayykoe <117815509+Wayykoe@users.noreply.github.com>
2026-01-30 10:24:29 +0100

    main

--- esgi.4a.render.nodejs/esgi.4a.render.nodejs-master/db.js
+++ esgi.4a.render.nodejs/esgi.4a.render.nodejs-master/db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-    'postgres://DB_USER:DB_PASSWORD@DB_HOST/DB_NAME',
+    'DATABASE_URL',
   {
     dialect: 'postgres',
     dialectOptions: {
